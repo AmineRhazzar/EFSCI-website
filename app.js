defaf125516b00ce3da396aa8a0ee4621926f06d
Mohammed El Amine Rhazzar <mohammedelaminerhazzar@gmail.com>
2021-05-23 13:31:03 +0100

    .

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
 const setHeaderHeight = () => {
-    const headerImg = document.querySelector(".header-img");
+    const headerImg = document.querySelector(".header-bg");
     document.querySelector(".header").style.height =
         headerImg.clientHeight + "px";
 };
+
 window.addEventListener("load", () => {
     setHeaderHeight();
 });
 window.addEventListener("resize", () => {
     setHeaderHeight();
 });
+
+
