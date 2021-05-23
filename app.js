6d651bb6309c460f9b1be6f32f555cf27bce7e7e
Amine <mohammedeleaminerhazzar@gmail.com>
2021-05-23 23:18:08 +0100

    .

--- app.js
+++ app.js
@@ -6,14 +6,21 @@ const setHeaderHeight = () => {
 
 window.addEventListener("load", () => {
     setHeaderHeight();
+    document.documentElement.scrollTop = 0;
+    // var Scrollbar = window.Scrollbar;
+    // Scrollbar.init(document.querySelector("#my-scrollbar"), {
+    //     continuousScrolling: false,
+    // });
+    setInterval(()=> {
+        if (document.querySelector("scroll-content")) {
+            document.querySelector("scroll-content").style.transform = "translate3d(0px, 0px, 0px)"
+        }
+        
+    }, 1000)
+    
 });
 window.addEventListener("resize", () => {
     setHeaderHeight();
 });
 
-var Scrollbar = window.Scrollbar;
 
-Scrollbar.init(document.querySelector("#my-scrollbar"), {
-    damping: 0.1,
-    continuousScrolling: false
-})
